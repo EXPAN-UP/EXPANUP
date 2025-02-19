90e796090504e669c4b484d9cffb271651a3ab5d
EXPAN-UP <shrutijaiswal936@gmail.com>
2025-02-19 11:19:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-//base by DGXeon (Xeon Bot Inc.)
-//YouTube: @DGXeon
-//Instagram: unicorn_xeon13
-//Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
-
 require('./settings')
 const makeWASocket = require("@whiskeysockets/baileys").default
 const { uncache, nocache } = require('./lib/loader')
